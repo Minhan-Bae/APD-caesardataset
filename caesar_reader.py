3b3f53b5d31809ae54df4255370fd7bed5d49a5b
Minhan-Bae <yuyuqw92@gmail.com>
2022-05-25 13:42:19 +0900

    feat: add else case(not mesh)

--- caesar_reader.py
+++ caesar_reader.py
@@ -1,16 +1,32 @@
 import os
 from random import choice
 from scipy import io
+import numpy as np
 
-file_dir = './caesar/caesar-fitted-meshes'
-file_list = os.listdir(file_dir)
-file_name = choice(file_list)
+np.set_printoptions(threshold=np.inf)
 
-mat_file = io.loadmat(os.path.join(file_dir,file_name))
+file_dir = './caesar/caesar'
+# file_dir = './caesar/caesar-fitted-meshes'
 
-# print(mat_file.keys()) # caesar file has __header__, __version__, __globals__, __points__
-print(f"name :          {file_name}")
-print(f"header  :       {mat_file['__header__']}")
-print(f"version :       {mat_file['__version__']}")
-print(f"globals :       {mat_file['__globals__']}") # empty
-print(f"n_points :      {len(mat_file['points'])}") # numpy array
+if file_dir.endswith('meshes'):
+    file_list = os.listdir(file_dir)
+    file_name = choice(file_list)
+    mat_file = io.loadmat(os.path.join(file_dir,file_name))
+    
+    # print(mat_file.keys()) # caesar file has __header__, __version__, __globals__, points
+    print(f"name :          {file_name}")
+    print(f"header  :       {mat_file['__header__']}")
+    print(f"version :       {mat_file['__version__']}")
+    print(f"globals :       {mat_file['__globals__']}") # empty
+    print(f"n_points :      {len(mat_file['points'])}") # numpy array
+
+else: #evaluation case
+    file_name = 'landmarksIdxs73.mat'
+    mat_file = io.loadmat(os.path.join(file_dir,file_name))
+
+    print(mat_file.keys()) # caesar file has __header__, __version__, __globals__, evalues
+    # print(f"name :          {file_name}")
+    # print(f"header  :       {mat_file['__header__']}")
+    # print(f"version :       {mat_file['__version__']}")
+    # print(f"globals :       {mat_file['__globals__']}") # empty
+    # print(f"n_points :      {mat_file['landmarksIdxs']}") # numpy array
