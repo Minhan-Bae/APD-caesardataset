3760334255c4419a6d01ff2517de322312239bfb
Minhan-Bae <yuyuqw92@gmail.com>
2022-05-25 11:55:56 +0900

    feat: CAESAR 데이터셋 로드 및 파일 구성 확인 코드 작성

---
+++ load_caesar.py
@@ -0,0 +1,17 @@
+import os
+from random import choice
+from scipy import io
+
+file_dir = './caesar/caesar-fitted-meshes'
+file_list = os.listdir(file_dir)
+file_name = choice(file_list)
+
+mat_file = io.loadmat(os.path.join(file_dir,file_name))
+
+# print(mat_file.keys()) # caesar file has __header__, __version__, __globals__, __points__
+print(f"name :          {file_name}")
+print(f"header  :       {mat_file['__header__']}")
+print(f"version :       {mat_file['__version__']}")
+print(f"globals :       {mat_file['__globals__']}") # empty
+print(f"n_points :      {len(mat_file['points'])}")
+
