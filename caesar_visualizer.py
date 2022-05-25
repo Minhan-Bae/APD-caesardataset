d26e6b47b3bafab3ac53eed5b3696eaa7c2f6e7e
Minhan-Bae <yuyuqw92@gmail.com>
2022-05-25 12:45:39 +0900

    feat: add caesar_visualizaer.py

---
+++ caesar_visualizer.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import numpy as np
+from vedo import *
+
+from scipy import io
+from random import choice
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
+
+file_dir = './caesar/caesar-fitted-meshes'
+file_list = os.listdir(file_dir)
+file_name = choice(file_list)
+
+mat_file = io.loadmat(os.path.join(file_dir,file_name))
+
+mat_file = mat_file['points']
+
+rpts = Points(mat_file, c=(0,0,0), r=5)
+show(rpts, axes=1)
+
+# for d in (0, 90, 180, 270):
+#     file_name = rpts.rotateY(d)
+#     file_name.show(interactive=True)
